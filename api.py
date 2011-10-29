3e32b83b5c47a47f57c06f98a1505bcc6495b54a
nanfang <nanfang@toureet.com>
2011-10-29 21:42:00 +0800

    implement api to get battle informations

--- api.py
+++ api.py
@@ -1,37 +1,77 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function, division
 import asyncore
 import json
 import logging
+from wsgiref.handlers import BaseHandler
 import os
 
 import tornado.httpserver
+from tornado.web import RequestHandler
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
 import tornado.options
 from tornado.options import define, options
+
 from models import *
 
 LISTENERS = []
 logger = logging.getLogger(__name__)
 
-class BattleListHandler(tornado.websocket.WebSocketHandler):
+class BattleListHandler(RequestHandler):
+  """
+  [
+  {
+    bf_id: 12345,
+    fighters: [
+      {
+        'name': 'SH',
+        'photo': 'http://test.com/sh.jpg'
+      },
+      {
+        'name': 'BJ',
+        'photo': 'http://test.com/bj.jpg'
+      }
+    ]
+    description: 'describe this battle field',
+    participants: 12300, // how many participants,
+    status: 0|1|2, // 0: not started, 1: ongoing, 2: finished
+    winner: 0/1/2, // which side wins, 0 means no winner
+  }
+]
+  """
   def get(self):
-    self.render('index.html')
+    self.set_header('Content-Type', 'application/json;charset=utf-8')
+    battles = [b.to_dict() for b in Battle.get_list()]
+    self.write(json.dumps(battles))
 
-class BattleHandler(tornado.websocket.WebSocketHandler):
+class BattleHandler(RequestHandler):
   def get(self, bf_id):
-    pass
+    self.set_header('Content-Type', 'application/json;charset=utf-8')
+    self.write(json.dumps(Battle.objects.get(id=bf_id).to_dict(detail=True)))
 
-class PostListHandler(tornado.websocket.WebSocketHandler):
-  def get(self, bf_id, fighter_id, type):
-    pass
+class PostListHandler(RequestHandler):
+  """
+  [{
+    post_id: 123456, // id for the post,
+    author_id: 3445236, // weibo account id,
+    author_name: xxx
+    author_avatar: xxx
+    comment: "this is cool", // comment user posted.
+    photo_url: "http://test.com/test.jpg",
+  }]
+  """
+  def get(self, bf_id, fighter, type):
+    self.set_header('Content-Type', 'application/json;charset=utf-8')
+    battle = Battle.objects.get(id=bf_id)
+    self.write(json.dumps([{p.to_dict()} for p in Post.battle_posts(battle, int(fighter), type)]))
 
-class PostMessageHandler(tornado.websocket.WebSocketHandler):
+class PostMessageHandler(RequestHandler):
   def post(self):
     pass
 
-class PostVoteHandler(tornado.websocket.WebSocketHandler):
+class PostVoteHandler(RequestHandler):
   def post(self):
     data = json.loads(self.request.body)
     post_id = data.get('post_id')
--- models.py
+++ models.py
@@ -3,6 +3,8 @@ from __future__ import division, unicode_literals, print_function
 from datetime import datetime
 from mongoengine.document import Document, EmbeddedDocument
 from mongoengine.fields import StringField, DateTimeField, IntField, EmbeddedDocumentField, ListField, ReferenceField
+from mongoengine.queryset import Q
+from const import BATTLE_ONGOING, BATTLE_FINISHED, BATTLE_NOT_STARTED
 
 """
 USER = hash{
@@ -40,10 +42,31 @@ class User(Document):
 
 class Fighter(EmbeddedDocument):
   name=StringField()
-  avatar = StringField()
-  blood = IntField(default=10000)
+  photo = StringField()
+  blood = IntField(default=1000)
   desc = StringField()
 
+  def to_dict(self):
+    """
+    name: 'BJ',
+    current_photo: 'http://test.com/bj.jpg',
+    description: 'BJ is bla bla bla...',
+    blood: 10, // how many blood left
+    recent_posts: [{
+      // see API 1.3
+    }],
+    hottest_posts: [{
+    }]
+    }, {          // city 1
+
+    }]
+    """
+    return {
+        'name':self.name,
+        'photo':self.photo,
+        'blood':self.blood,
+        'desc':self.desc,
+      }
 
 class Battle(Document):
   start=DateTimeField()
@@ -53,27 +76,111 @@ class Battle(Document):
   fighter1 = EmbeddedDocumentField(Fighter)
   fighter2 = EmbeddedDocumentField(Fighter)
 
-#  participants = ListField(ReferenceField(User), default=lambda :[])
-  participants_num = IntField()
-
+  participants = IntField()
   created_at = DateTimeField(default=datetime.now)
 
+  @property
+  def status(self):
+    if datetime.now() < self.start:
+      return BATTLE_NOT_STARTED
+    if datetime.now() > self.end:
+      return BATTLE_FINISHED
+    return BATTLE_ONGOING
 
-class Post(Document):
+  @property
+  def winner(self):
+    if self.status == BATTLE_FINISHED:
+      return 1 if self.fighter1.blood > self.fighter2.blood else 2
+    return 0
+
+
+  def to_dict(self, detail=False):
+    """
+    bf_id: 12345, //which battle field this is for
+    description: 'describe this battle field',
+    participants: 12300,
+    winner: 0/1,
+    time_left: 10800, // how many seconds this battle will be over
+    fighters: [{  // city 0
+    name: 'BJ',
+    current_photo: 'http://test.com/bj.jpg',
+    description: 'BJ is bla bla bla...',
+    blood: 10, // how many blood left
+    recent_posts: [{
+      // see API 1.3
+    }],
+    hottest_posts: [{
+    }]
+    }, {          // city 1
+
+    }]
+  """
+    info= {
+      'bf_id':str(self.id),
+      'description':self.desc,
+      'participants':self.participants,
+      'status':self.status,
+      'winner':self.winner,
+      'time_left':self.time_left,
+      'fighter1':self.fighter1.to_dict(),
+      'fighter2':self.fighter2.to_dict(),
+    }
+    if detail:
+      info['fighter1'].update({
+        'recent_posts':Post.battle_posts(self,1,'recent'),
+        'hottest_posts':Post.battle_posts(self,1,'hottest')
+      })
+      info['fighter2'].update({
+        'recent_posts':Post.battle_posts(self,2,'recent'),
+        'hottest_posts':Post.battle_posts(self,2,'hottest')
+      })
+
+    return info
+
+  @classmethod
+  def get_list(cls):
+    return cls.objects.order_by('-participants', '-created_at')
+
+class  Post(Document):
   battle = ReferenceField(Battle)
   author = ReferenceField(User)
-  post_for = IntField()
+  fighter = IntField()
 
   voters = ListField(ReferenceField(User))
   votes = IntField()
 
-  content = StringField()
-  photo = StringField()
+  comment = StringField()
+  photo_url = StringField()
 
   created_at = DateTimeField(default=datetime.now)
 
+  @classmethod
+  def battle_posts(cls, battle, fighter, type):
+    if type=='recent':
+      return Post.objects(Q(battle=battle)&Q(fighter=fighter)).order_by('-created_on')[:5]
+    return Post.objects(Q(battle=battle)&Q(fighter=fighter)).order_by('-votes')[:5]
 
-
+  def to_dict(self):
+    """
+    {
+    post_id: 123456, // id for the post,
+    author_id: 3445236, // weibo account id,
+    author_name: xxx
+    author_avatar: xxx
+    comment: "this is cool", // comment user posted.
+    photo_url: "http://test.com/test.jpg",
+    votes: 178
+    }
+    """
+    return {
+      'post_id':str(self.id),
+      'author_id':str(self.author.id),
+      'author_name':self.author.name,
+      'author_avatar':self.author.avatar,
+      'comment':self.comment,
+      'photo_url':self.photo_url,
+      'votes':self.votes
+    }
 
 
 
