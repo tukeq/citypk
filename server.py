e1e12f9ed48034e37ecc15f170631bd4f171d74e
nanfang <nanfang@toureet.com>
2011-10-29 19:52:23 +0800

    add models

--- server.py
+++ server.py
@@ -14,7 +14,6 @@ from mongoengine import connect
 import const
 from api import *
 
-
 LISTENERS = []
 logger = logging.getLogger(__name__)
 
@@ -35,7 +34,7 @@ class RealtimeHandler(tornado.websocket.WebSocketHandler):
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self, *args, **kargs):
-        self.render('index.html')
+        self.render('index.html', host=options.host, port=options.port)
 
     def post(self, *args, **kwargs):
 
@@ -43,55 +42,6 @@ class IndexHandler(tornado.web.RequestHandler):
 #        print(self.request.)
         for listener in LISTENERS:
           listener.write_message('this is from server, websocket is okay')
-
-
-"""
-
-
-USER = hash{
-    id
-    name
-    avatar
-}
-
-
-BATTLE = hash{
-  id
-  start
-  end
-  description
-  status
-  fighters = [{id0, photo, blood, desc, posts}, {id1, photo, blood, desc}]
-  winner
-  participants=[user_id,...]
-}
-
-POSTS
-{
-id
-battle_id
-content
-photo_url
-voters=[user_id,...]
-created_at
-}
-
-
-
-
-
-
-
-"""
-
-
-# API: get battle field list
-"""
-{
-
-}
-"""
-
 
 def start_web():
     settings = dict(
@@ -112,12 +62,9 @@ def start_web():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
+    print('start server at %s:%s' % (options.host, options.port))
     tornado.ioloop.IOLoop.instance().start()
 
-
 if __name__ == "__main__":
-    print('server start on %s' % options.port)
-    connect('citypk')
+    connect(db='citypk')
     start_web()
-
-
